52113d2f883b504cc94e1b64a8ab4b82ea9a26f3
Lucas do Nascimento Ribeiro <lucas.ribeiro@embedded.ufcg.edu.br>
2015-04-27 17:51:45 -0300

    Add discovery function

--- app/src/main/java/guri/br/selfiestudio/MainActivity.java
+++ app/src/main/java/guri/br/selfiestudio/MainActivity.java
@@ -3,7 +3,10 @@ package guri.br.selfiestudio;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,13 +23,15 @@ import java.util.Set;
 
 public class MainActivity extends Activity {
 
+    //Declaring variables
     private static final int REQUEST_ENABLE_BT = 1;
-    BluetoothAdapter mBluetoothAdapter;
-    Button turnOn, turnOff, setVisible, discovery, showPairedDevices;
-    ListView listView;
-    ArrayList<BluetoothDevice> devices;
-    Set<BluetoothDevice> pairedDevices;
-    ArrayAdapter mArrayAdapter;
+    private BluetoothAdapter mBluetoothAdapter;
+    public Button turnOn, turnOff, setVisible, discovery, showPairedDevices;
+    public ListView listView;
+    private ArrayList<BluetoothDevice> devices;
+    private Set<BluetoothDevice> pairedDevices;
+    private ArrayAdapter mArrayAdapter;
+    private BroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,10 +67,12 @@ public class MainActivity extends Activity {
             }
         });
 
+        //Show paired devices in the list view.
         showPairedDevices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mBluetoothAdapter.isEnabled()){
+                    mArrayAdapter.clear();
                     // If there are paired devices
                     if (pairedDevices.size() > 0) {
                         // Loop through paired devices
@@ -82,18 +89,57 @@ public class MainActivity extends Activity {
 
             }
         });
+
+        discovery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBluetoothAdapter.isEnabled()){
+                    mArrayAdapter.clear();
+                    mBluetoothAdapter.startDiscovery();
+                }
+            }
+        });
     }
 
+    /**
+     * Initiate variables.
+     */
     private void init(){
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         turnOn = (Button) findViewById(R.id.on_button);
         turnOff = (Button) findViewById(R.id.off_button);
         showPairedDevices = (Button) findViewById(R.id.paired_devices_button);
         setVisible = (Button) findViewById(R.id.visibility_button);
+        discovery = (Button) findViewById(R.id.discovery_button);
         devices = new ArrayList<BluetoothDevice>();
         pairedDevices = mBluetoothAdapter.getBondedDevices();
         listView = (ListView) findViewById(R.id.devices_list_view);
         mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(mArrayAdapter);
+
+        // Create a BroadcastReceiver for ACTION_FOUND
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
     }
 }
