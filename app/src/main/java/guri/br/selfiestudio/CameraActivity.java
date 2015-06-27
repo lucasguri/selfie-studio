262750d06bd6877fb00a188b95652b646788334f
Lucas do Nascimento Ribeiro <lucas.ribeiro@embedded.ufcg.edu.br>
2015-06-27 13:53:04 -0300

    Calcula tamanho da imagem a ser enviada

--- app/src/main/java/guri/br/selfiestudio/CameraActivity.java
+++ app/src/main/java/guri/br/selfiestudio/CameraActivity.java
@@ -139,11 +139,14 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
                             });
                     // escrevendo o array de bytes da foto pelo outputstream
                     if (MainActivity.mThreadComunicacao != null){
-                        BitmapFactory bitmapFactory = new BitmapFactory();
-                        BitmapFactory.Options options = new BitmapFactory.Options();
-                        options.inJustDecodeBounds = false;
-                        options.inSampleSize = 8;
-                        Bitmap image = bitmapFactory.decodeByteArray(arg0, 0, arg0.length, options);
+                        //BitmapFactory bitmapFactory = new BitmapFactory();
+                        //BitmapFactory.Options options = new BitmapFactory.Options();
+                        //options.inJustDecodeBounds = false;
+                        //options.inSampleSize = 8;
+                        //Bitmap image = bitmapFactory.decodeByteArray(arg0, 0, arg0.length, options);
+
+                        //
+                        Bitmap image = decodeSampledBitmapFromByteArray(arg0, 1136, 640);
 
                         ByteArrayOutputStream stream = new ByteArrayOutputStream();
                         image.compress(Bitmap.CompressFormat.JPEG, 60, stream);
@@ -173,6 +176,44 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
                 }
             }
         }};
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromByteArray(byte[] bytes, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+    }
 
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
